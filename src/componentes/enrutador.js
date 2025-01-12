5aefad94da62dcd5d1933c72d43a1f29e95f1b84
KevinJHP19 <huamanpintokevin@fpllefia.com>
2025-01-12 21:18:29 +0000

    Componente enrutador

---
+++ src/componentes/enrutador.js
@@ -0,0 +1,19 @@
+export const enrutador = {
+    rutas: {
+      home: import('../vistas/homeVista.js'),
+      // Usuarios
+      admin: import('../vistas/adminVista.js'),
+      registro: import('../vistas/registroVista.js'),
+      login: import('../vistas/loginVista.js'),
+      // Proyectos
+      proyectos: import('../vistas/proyectosVista.js'),
+      proyectoNuevo: import('../vistas/proyectoNuevoVista.js'),
+      proyectoEditar: import('../vistas/proyectoEditarVista.js'),
+      proyectoDetalle: import('../vistas/proyectoDetalleVista.js'),
+      404: import('../vistas/404.js')
+    },
+    router: () => {},
+    observadorRutas: () => {}
+  }
+  
+  
