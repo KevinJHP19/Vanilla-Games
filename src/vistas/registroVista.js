333e0e0ce043ea27f449ec4a585f72903223c522
KevinJHP19 <huamanpintokevin@fpllefia.com>
2025-01-12 20:57:20 +0000

    tarea hecha de vistas

--- src/vistas/registroVista.js
+++ src/vistas/registroVista.js
@@ -28,5 +28,23 @@ export default {
                         <input type="submit" class="btn btn-primary w-100 mt-3" value="Enviar" />
                     </form>
                 </div>
-            </div>`
+            </div>`,
+            script: () => {
+                console.log('vista registro cargada')
+                // Validación bootstrap
+            
+                // Capturamos el formulario en una variable
+                const formulario = document.querySelector('form')
+                // Detectamos su evento submit (enviar)
+                formulario.addEventListener('submit', (event) => {
+                  // Detenemos el evento enviar (submit)
+                    event.preventDefault()
+                    event.stopPropagation()
+                // Comprobamos si el formulario no valida
+                  if (!formulario.checkValidity()) {
+                    // Y añadimos la clase 'was-validate' para que se muestren los mensajes
+                    formulario.classList.add('was-validated')
+                  }
+                })
+              }
 }
